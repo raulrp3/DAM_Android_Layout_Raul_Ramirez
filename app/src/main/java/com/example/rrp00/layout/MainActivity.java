363938004a21d9559133d8c02771dc472cfa75c7
raulrp3 <raul.ramirez@escuelaestech.es>
2019-01-20 12:57:07 +0100

    First commit

---
+++ app/src/main/java/com/example/rrp00/layout/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.rrp00.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.frame_layout);
+        setContentView(R.layout.linear_layout);
+    }
+}
